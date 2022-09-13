463046871ecb1ba7e14a0b1e269865ef2d3808f4
Lucas Dzin Pedroso <lucasdzin@gmail.com>
2022-09-13 13:04:13 -0300

    Landing page layout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,32 @@
-import { View } from 'react-native';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter";
+
+import { Background } from "./src/components/Background";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View>
-      
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Home /> : <Loading /> }
+    </Background>
   );
 }
